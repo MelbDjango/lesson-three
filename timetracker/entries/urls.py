d988c89186d9f6baf652bcc2b80d5b6f6cd80db2
Markus Holtermann <info@markusholtermann.eu>
2015-08-07 11:28:46 +1000

    Added solution for lesson three

--- timetracker/entries/urls.py
+++ timetracker/entries/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 
-from entries import views
+from . import views
 
 
 urlpatterns = [
-    url(r'^entries/$', views.entries, name='entries'),
+    url(r'^clients/$', views.clients, name='client-list'),
+    url(r'^clients/(?P<pk>\d+)/$', views.client_detail, name='client-detail'),
+    url(r'^entries/$', views.entries, name='entry-list'),
+    url(r'^projects/$', views.projects, name='project-list'),
 ]
