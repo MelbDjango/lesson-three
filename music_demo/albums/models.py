34c6986bac7e9fdcde6edf052b57e3cdcc11fa87
Markus Holtermann <info@markusholtermann.eu>
2016-01-14 13:53:28 +1100

    Updated music_demo

--- music_demo/albums/models.py
+++ music_demo/albums/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Album(models.Model):
     name = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
--- music_demo/albums/views.py
+++ music_demo/albums/views.py
@@ -3,5 +3,5 @@ from .models import Album
 
 
 def albums(request):
-  album_list = Album.objects.all()
-  return render(request, 'albums.html', {'album_list': album_list})
+    album_list = Album.objects.all()
+    return render(request, 'albums.html', {'album_list': album_list})
